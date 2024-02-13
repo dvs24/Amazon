07eb085c7d9b8510c1e8d7c1c7b9906a66078c2e
DVS <divyesh220998@gmail.com>
2024-02-13 22:45:43 +0530

    Added Navabar and subNavbar

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,25 +1,45 @@
-import React from 'react'
-import styles from './Navbar.module.css';
-import Image from 'next/image'
-import logo from '../../assets/logo.svg'
+import React from "react";
+import styles from "./Navbar.module.css";
+import Image from "next/image";
+import logo from "../../assets/logo.svg";
+import cart from "../../assets/cartIcon.png";
 
 const Navbar = () => {
   return (
     <div className={styles.navBar}>
-        <div className={styles.logoImg}>
-        <Image src={logo} width={100} height = {30}/>
-        </div>
+      <div className={styles.logoImg}>
+        <Image src={logo} width={100} height={60} />
+      </div>
 
-        <div className={styles.deliveryOption}>
-            <div className={styles.deliveryText}>
-                Delivery
-            </div>
-            <div className={styles.country}>
-                India
-            </div>
-        </div>
+      <div className={styles.textWrapper}>
+        <div className={styles.deliveryText}>Delivery</div>
+        <div className={styles.country}>India</div>
+      </div>
+
+      <div className={styles.searchWrapper}>
+        <input
+          type="text"
+          className={styles.searchInput}
+          placeholder="Search Products"
+        />
+        <button className={styles.searchBtn}>search</button>
+      </div>
+
+      <div className={styles.textWrapper}>
+        <div className={styles.signInText}>Hello Sign in</div>
+        <div className={styles.AccList}>Accounts & Lists</div>
+      </div>
+      <div className={styles.textWrapper}>
+        <div className={styles.markedText}>Marked</div>
+        <div className={styles.favouritesText}>& Favourites</div>
+      </div>
+
+      <div className={styles.cartWrapper}>
+          <Image src={cart} width={30} height={20} />
+        <div className={styles.cartText}>cart</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ src/app/components/SubNavbar/SubNavbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import styles from './SubNavbar.module.css';
+import menu from '../../assets/menu.svg'
+import Image from "next/image";
+
+
+const SubNavbar = () => {
+  return (
+    <div className={styles.subNavbar}>
+        <Image src={menu} width={25} height = {25} />
+        <button className={styles.subNavbarBtn}>
+            All
+        </button>
+        <button className={styles.subNavbarBtn}>
+            Today's Deals
+        </button>
+        <button className={styles.subNavbarBtn}>
+            Customer Service
+        </button>
+        <button className={styles.subNavbarBtn}>
+            Registry
+        </button>
+        <button className={styles.subNavbarBtn}>
+            Gift Cards
+        </button>
+        <button className={styles.subNavbarBtn}>
+            Sell
+        </button>
+    </div>
+  )
+}
+
+export default SubNavbar
